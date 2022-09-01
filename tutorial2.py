b263394fd2e9024b0ba984b255b0b0a773822ddf
richwill28 <e0550368@u.nus.edu>
2022-09-01 15:37:22 +0800

    Update tutorial 2

--- tutorial2.py
+++ tutorial2.py
@@ -6,29 +6,31 @@ graph = {
 }
 
 def hstar(node):
-    # TODO Implement this, can just hardcode here.
-    raise NotImplementedError
+    return {'S': 9, 'A': 3, 'B': 2, 'G': 0}[node]
+
 def h1(node):
     return 0
+
 def h2(node):
-    return {'S':8, 'A':1, 'B':1, 'G':0}[node]
+    return {'S': 8, 'A': 1, 'B': 1, 'G': 0}[node]
+
 def h3(node):
-    return {'S':9, 'A':3, 'B':2, 'G':0}[node]
+    return {'S': 9, 'A': 3, 'B': 2, 'G': 0}[node]
+
 def h4(node):
-    return {'S':6, 'A':3, 'B':1, 'G':0}[node]
+    return {'S': 6, 'A': 3, 'B': 1, 'G': 0}[node]
+
 def h5(node):
-    return {'S':8, 'A':4, 'B':2, 'G':0}[node]
+    return {'S': 8, 'A': 4, 'B': 2, 'G': 0}[node]
+
 def h_q5d(node):
-    # TODO Implement this
-    raise NotImplementedError
+    return max(h3(node), h5(node))
 
 def admissible_constraint(h_val, hstar_val, **kwargs):
     return h_val <= hstar_val
 
 def consistent_constraint(h_val, h_val_successor, cost, **kwargs):
     return h_val <= cost + h_val_successor
-    # TODO Implement this
-    raise NotImplementedError
 
 def print_constraint(h_fns, constraint_fns, hstar_fn, show_violation=False):
     for h_fn in h_fns:
@@ -47,16 +49,25 @@ print_constraint([h1, h2, h3, h4, h5, h_q5d], [admissible_constraint, consistent
 
 from collections import defaultdict
 from priority_queue import PriorityQueue # This is the same one as in AIMA
-def astar_search(graph, h, goal_test, explored_order):
 
+def astar_search(graph, h, goal_test, explored_order):
     cost_fn_g = defaultdict(lambda: 0)
     f = lambda node: cost_fn_g[node] + h(node)
     node = 'S'
     frontier = PriorityQueue('min', f)
     frontier.append(node)
     explored = set()
-    # TODO Implement this
-    raise NotImplementedError
+    while len(frontier) > 0:
+        current = frontier.pop()
+        explored.add(current)
+        explored_order.append(current)
+        if goal_test(current):
+            break
+        for successor_node, cost in graph[current]:
+            cost_fn_g[successor_node] = cost_fn_g[current] + cost
+            # assuming g(n) won't get updated again, then it is safe to just append to frontier
+            if successor_node not in explored:
+                frontier.append(successor_node)
 
 print("A*STAR - Order of the nodes that are explored.")
 explored_order = []
